29f62e939eb458ab2abed541311a6468f0c24fbf
Ranjani S <ranjanis@Janis-MacBook-Air.local>
2026-06-16 10:08:56 +0530

    Add email and scheduler modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmailModule } from './email/email.module';
+import { SchedulerModule } from './scheduler/scheduler.module';
 
 @Module({
-  imports: [],
+  imports: [EmailModule, SchedulerModule],
   controllers: [AppController],
   providers: [AppService],
 })
